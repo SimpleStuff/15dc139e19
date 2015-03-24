075083cd5c26a9c9b0032c2d4c592f5e5828c076
zclj <user@server43.fake>
2015-03-24 20:01:29 +0100

    First version of export of competition data

--- src/tango/import.clj
+++ src/tango/import.clj
@@ -77,7 +77,67 @@
          []))
       (create-import-info "" [] :failed [:file-not-found]))))
 
+;{:tag :item, :attrs {:id "1"}, :content ["First"]}
+;(spit "test.xml" (with-out-str (clojure.xml/emit simple-xml-map)))
+(def small-exampel-data
+  {:file/version "4.1",
+   :file/content
+   {:competition/name "TurboMegatävling",
+    :competition/location "THUNDERDOME",
+    :competition/date #inst "2014-11-22T00:00:00.000-00:00",
+    :competition/classes
+    [{:class/name "Hiphop Singel Star B", :class/competitors
+      [{:competitor/name "Rulle Trulle", :competitor/number 1, :competitor/club "Rulles M&M"}
+       {:competitor/name "Katchyk Wrong", :competitor/number 2, :competitor/club "Sccchhh"}]}
+     {:class/name "Hiphop Singel Star J Fl", :class/competitors
+      [{:competitor/name "Ringo Stingo", :competitor/number 20, :competitor/club "Kapangg"}
+       {:competitor/name "Greve Turbo", :competitor/number 21, :competitor/club "OOoost"}]}]},
+   :file/import-status :success, :file/import-errors []})
+
+ ;; <Class Name="Hiphop Singel Star B" Seq="0">
+ ;;      <StartList Qty="2">
+ ;;        <Couple Name="Rulle Trulle" Seq="0" License="" Club="Rulles M&amp;M" Number="1"/>
+ ;;        <Couple Name="Katchyk Wrong" Seq="1" License="" Club="Sccchhh" Number="2"/>
+ ;;      </StartList>
+ ;;    </Class>
+
+(defn competitor->xml [competitor seq-nr]
+  {:tag :Couple :attrs {:Name (:competitor/name competitor)
+                        :Seq seq-nr
+                        :License ""
+                        :Club (:competitor/club competitor)
+                        :Number (:competitor/number competitor)}})
+
+(defn competitors->dance-perfect-xml-start-list [competitiors]
+  [{:tag :StartList :attrs {:Qty (count competitiors)} :content
+    (reduce (fn [start-list-xml competitor]
+              (conj start-list-xml (competitor->xml competitor (count start-list-xml))))
+            []
+            competitiors)}])
+
+(defn classes->dance-perfect-xml-classes [classes]
+  (reduce (fn [classes-xml class]
+            (conj classes-xml {:tag :Class :attrs
+                               {:Name (:class/name class)
+                                :Seq (count classes-xml)}
+                               :content (competitors->dance-perfect-xml-start-list (:class/competitors class))}))
+          []
+          class-ex))
+
+(defn date->dance-perfect-format [date]
+  (.format (java.text.SimpleDateFormat. "yyyy-MM-dd") date))
+
+(defn export-file [version dance-perfect-data]
+  (xml/emit {:tag :DancePerfect :attrs {:Version version} :content
+             [{:tag :CompData :attrs {:Name (:competition/name dance-perfect-data)
+                                      :Date (date->dance-perfect-format (:competition/date dance-perfect-data))
+                                      :Place (:competition/location dance-perfect-data)}}
+              {:tag :AdjPanelList :content [{:tag :AdjList}]}
+              {:tag :ClassList :content
+               (classes->dance-perfect-xml-classes (:competition/classes dance-perfect-data))}]}))
+
 (defn import-file-stream [{:keys [content]}]
   (dance-perfect-xml->data (read-xml-string content)))
 
+;(spit "export.xml" (with-out-str (export-file 4.1 (:file/content small-exampel-data))))
 ;http://blog.fogus.me/2011/09/08/10-technical-papers-every-programmer-should-read-at-least-twice/
---
+++ test/tango/export_tests.clj
@@ -0,0 +1,33 @@
+(ns tango.export-tests
+  (:require [clojure.test :refer :all]            
+            [tango.import :as imp]
+            [clj-time.core :as tc]
+            [clj-time.coerce :as tcr]
+            [clojure.xml :as xml]
+            [tango.test-data :as data]))
+
+; (clojure.test/run-tests 'tango.export-tests)
+
+(declare small-exampel-data)
+
+(deftest export-dance-perfect-file
+  (testing "Export of a Dance Perfect xml file"
+    (let [imported-file (imp/import-file "./test/tango/small-example.xml")
+          exported-file (imp/export-file imported-file)]
+      (is (= (imp/read-xml "./test/tango/small-example.xml")
+             exported-file)))))
+
+(def small-exampel-data
+  {:file/version "4.1",
+   :file/content
+   {:competition/name "TurboMegatävling",
+    :competition/location "THUNDERDOME",
+    :competition/date #inst "2014-11-22T00:00:00.000-00:00",
+    :competition/classes
+    [{:class/name "Hiphop Singel Star B", :class/competitors
+      [{:competitor/name "Rulle Trulle", :competitor/number 1, :competitor/club "Rulles M&M"}
+       {:competitor/name "Katchyk Wrong", :competitor/number 2, :competitor/club "Sccchhh"}]}
+     {:class/name "Hiphop Singel Star J Fl", :class/competitors
+      [{:competitor/name "Ringo Stingo", :competitor/number 20, :competitor/club "Kapangg"}
+       {:competitor/name "Greve Turbo", :competitor/number 21, :competitor/club "OOoost"}]}]},
+   :file/import-status :success, :file/import-errors []})
