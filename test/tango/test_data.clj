634117089d4c065d71178148bbcfad3dc8578ef9
zclj <user@server43.fake>
2015-09-27 16:19:30 +0200

    fixed failing tests due to result-example test data missing DP flag

--- test/tango/test_data.clj
+++ test/tango/test_data.clj
@@ -58,6 +58,7 @@
   {:competition/name "TurboMegatävling"
    :competition/date (tcr/to-date (tc/date-time 2014 11 22))
    :competition/location "THUNDERDOME"
+   :dance-perfect/flags {:adj-order-final 1}
    :competition/classes
    [{:class/name "Hiphop Singel Star B"
      :class/position 0
