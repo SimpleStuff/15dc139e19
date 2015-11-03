fa102668ba92e7237b123e995be833f376bf51c0
zclj <info-s@tryne.se>
2015-11-03 20:11:56 +0100

    wip on import for new model

--- src/tango/import_new.clj
+++ src/tango/import_new.clj
@@ -6,7 +6,20 @@
             [clojure.zip :as zip]
             [clojure.data.zip.xml :as zx]))
 
-;; todo - prefix
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Utils
+(defn- to-number [s]
+  {:pre [(string? s)]}
+  (let [prepared-string (clojure.string/replace s #" " "")]
+    (cond (re-seq #"^[-+]?\d*[\.,]\d*$" prepared-string)
+          (Double/parseDouble (clojure.string/replace prepared-string #"," "."))
+          (re-seq #"^[-+]?\d+$" prepared-string)
+          (Integer/parseInt (clojure.string/replace prepared-string #"\+" ""))
+          :else s)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Makers
+
 (defn make-competition
   [name date location panels adjudicators activites classes]
   {:competition/name name
@@ -18,27 +31,106 @@
    :competition/classes classes ;[example-class-1]
    })
 
+(defn make-competition-data
+  [name date location]
+  {:competition/name name
+   :competition/date date ;(tcr/to-date (tc/date-time 2014 11 22))
+   :competition/location location})
+
+(defn make-adjudicator
+  [id name country]
+  {:adjudicator/id id
+   :adjudicator/name name
+   :adjudicator/country country})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Xml import
+
 (defn- get-name-attr [loc]
   (zx/attr loc :Name))
 
-(defn adjudicators->map [xml-loc]
-  (for [adjudicator (zx/xml-> xml-loc :AdjList :Adjudicator)]
-    {:adjudicator/name (zx/attr adjudicator :Name)}))
+(defn- adjudicators->map [adjudicator-loc id-generator-fn]
+  (assoc
+      (make-adjudicator
+       (id-generator-fn)
+       (zx/attr adjudicator-loc :Name)
+       (zx/attr adjudicator-loc :Country))
+    :dp/temp-id (to-number (zx/attr adjudicator-loc :Seq))))
 
-(defn competition->map [xml-loc]
-  (make-competition
+(defn- competition-data->map [xml-loc]
+  (make-competition-data
    (get-name-attr xml-loc)
    (tcr/to-date
     (tf/parse (tf/formatter "yyyy-MM-dd")
               (zx/attr xml-loc :Date)))
-   (zx/attr xml-loc :Place)
-   []
-   []
-   []
-   []))
+   (zx/attr xml-loc :Place)))
 
-(defn competition-xml->map [xml]
-  (first (zx/xml-> xml :CompData)))
+(defn- get-adj-number-attr [loc]
+  (to-number (zx/attr loc :AdjNumber)))
+
+(defn panel->map [panel-loc id-generator-fn adjudicators]
+  (for [panel panel-loc]
+    (dissoc
+     (first (filter #(= (get-adj-number-attr panel) (:dp/temp-id %))
+                    adjudicators))
+     :dp/temp-id)
+    ;; {:adjudicator-id (get-adj-number-attr panel)
+    ;;  }
+    ))
+
+;; (defn adjudicator-panel-list->map [adjudicator-panels-loc]
+;;   (filter
+;;    (fn [rec] (seq (rec :panel/adjudicators)))
+;;    (for [panel adjudicator-panels-loc]
+;;      {:panel/id 1;(get-seq-attr-no-inc panel)
+;;       :panel/adjudicators
+;;       (into [] (panel->map (zx/xml-> panel :PanelAdj)))})))
+
+	;; <Panel Seq="0" Qty="2">
+        ;;   <PanelAdj Seq="0" AdjNumber="0"/>
+        ;;   <PanelAdj Seq="1" AdjNumber="1"/>
+        ;; </Panel>
+
+(defn adjudicator-panel-list->map
+  ""
+  [xml-loc id-generator-fn adjudicators]
+  (filter
+   ;; Adjudicators can be empty and if so its an empty panel and should not
+   ;; be used
+   (fn [adjudicators] (seq (:adjudicator-panel/adjudicators adjudicators)))
+   (for [panel xml-loc]
+     {:adjudicator-panel/name (str (inc (to-number (zx/attr panel :Seq))))
+      :adjudicator-panel/id (id-generator-fn)          ;(get-seq-attr-no-inc panel)
+      :adjudicator-panel/adjudicators
+      (into [] (panel->map (zx/xml-> panel :PanelAdj) id-generator-fn adjudicators))})))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Import API
+
+(defn adjudicators-xml->map [xml id-generator-fn]
+  (mapv
+   #(adjudicators->map % id-generator-fn)
+   (zx/xml-> xml :AdjPanelList :AdjList :Adjudicator)))
+
+(defn adjudicator-panels-xml->map [xml id-generator-fn adjudicators]
+  (adjudicator-panel-list->map (zx/xml-> xml :AdjPanelList :PanelList :Panel) id-generator-fn adjudicators))
+
+(defn competition-data-xml->map [xml]
+  (competition-data->map (first (zx/xml-> xml :CompData))))
+
+(defn competition-xml->map [xml id-generator-fn]
+  (let [comp-data (competition-data-xml->map xml)
+        dp-adjudicators (adjudicators-xml->map xml id-generator-fn)]
+    (make-competition
+     (:competition/name comp-data)
+     (:competition/date comp-data)
+     (:competition/location comp-data)
+     (adjudicator-panels-xml->map xml id-generator-fn dp-adjudicators)
+     (mapv #(dissoc % :dp/temp-id) dp-adjudicators)
+     []
+     [])))
 
 ;; (defn competition->map [loc]
 ;;   (let [competition-data (first (zx/xml-> loc :CompData))
--- test/tango/import_tests_new.clj
+++ test/tango/import_tests_new.clj
@@ -19,23 +19,115 @@
 
 ; (def small-exampel-xml (xml/parse (clojure.java.io/file "./test/tango/small-example.xml")))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Setup
+
 (def competition-snippet
   (zip/xml-zip (xml/parse (clojure.java.io/file "./test/tango/test-snippets.xml"))))
 
-;; need to do first
+(def expected-lookup-table
+  {:adjudicator-index
+   {:0 1
+    :1 2
+    :2 3}})
+
+(def expected-adjudicators
+  [{:adjudicator/name "Anders"
+    :adjudicator/id 1
+    :adjudicator/country "Sweden"
+    :dp/temp-id 0}
+   {:adjudicator/name "Bertil"
+    :adjudicator/id 2
+    :adjudicator/country ""
+    :dp/temp-id 1}
+   {:adjudicator/name "Cesar"
+    :adjudicator/id 3
+    :adjudicator/country ""
+    :dp/temp-id 2}])
+
+(def expected-panels
+  [{:adjudicator-panel/name "1"
+    :adjudicator-panel/adjudicators
+    [{:adjudicator/name "Anders"
+      :adjudicator/id 1
+      :adjudicator/country "Sweden"}
+     {:adjudicator/name "Bertil"
+      :adjudicator/id 2
+      :adjudicator/country ""}]
+    :adjudicator-panel/id 1}
+
+   {:adjudicator-panel/name "2"
+    :adjudicator-panel/adjudicators
+    [{:adjudicator/name "Bertil"
+      :adjudicator/id 2
+      :adjudicator/country ""}
+     {:adjudicator/name "Cesar"
+      :adjudicator/id 3
+      :adjudicator/country ""}]
+    :adjudicator-panel/id 2}])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Tests
+
 (deftest import-adjudicator
   (testing "Import of adjudicator part"
-    (is (= (imp/adjudicators->map (zx/xml-> competition-snippet
-                                            :AdjPanelListSnippets :AdjPanelList))
-           [{:adjudicator/name "Anders"}]))))
+    (let [current-id (atom 0)]
+      (is (= (imp/adjudicators-xml->map competition-snippet #(swap! current-id inc))
+             expected-adjudicators)))))
+
+(deftest import-dance-perfect-panels
+  (testing "Import of adjudicator panels"
+    (let [current-id (atom 0)]
+      (is (= (imp/adjudicator-panels-xml->map
+              competition-snippet
+              #(swap! current-id inc)
+              expected-adjudicators)
+             expected-panels)))))
+
+(deftest import-dance-perfect-competition-data
+  (testing "Import competition data of Dance Perfect file"
+    (is (= (imp/competition-data-xml->map competition-snippet)
+           {:competition/name "TurboMegatävling"
+            :competition/date (tcr/to-date (tc/date-time 2014 11 22))
+            :competition/location "THUNDERDOME"}))))
 
 (deftest import-dance-perfect-competition
   (testing "Import competition name of Dance Perfect file"
-    (is (= (imp/competition->map (first (zx/xml-> competition-snippet :CompDataSnippets :CompData)))
-           {:competition/name "TurboMegatävling"
-            :competition/date (tcr/to-date (tc/date-time 2014 11 22))
-            :competition/location "THUNDERDOME"
-            :competition/panels []
-            :competition/adjudicators []
-            :competitor/activities []
-            :competition/classes []}))))
+    (let [current-id (atom 0)]
+      (is (= (imp/competition-xml->map competition-snippet #(swap! current-id inc))
+             {:competition/name "TurboMegatävling"
+              :competition/date (tcr/to-date (tc/date-time 2014 11 22))
+              :competition/location "THUNDERDOME"
+              :competition/panels
+              [{:adjudicator-panel/name "1"
+                :adjudicator-panel/adjudicators
+                [{:adjudicator/name "Anders"
+                  :adjudicator/id 1
+                  :adjudicator/country "Sweden"}
+                 {:adjudicator/name "Bertil"
+                  :adjudicator/id 2
+                  :adjudicator/country ""}]
+                :adjudicator-panel/id 4}
+               
+               {:adjudicator-panel/name "2"
+                :adjudicator-panel/adjudicators
+                [{:adjudicator/name "Bertil"
+                  :adjudicator/id 2
+                  :adjudicator/country ""}
+                 {:adjudicator/name "Cesar"
+                  :adjudicator/id 3
+                  :adjudicator/country ""}]
+                :adjudicator-panel/id 5}]
+
+              :competition/adjudicators
+              [{:adjudicator/name "Anders"
+                :adjudicator/id 1
+                :adjudicator/country "Sweden"}
+               {:adjudicator/name "Bertil"
+                :adjudicator/id 2
+                :adjudicator/country ""}
+               {:adjudicator/name "Cesar"
+                :adjudicator/id 3
+                :adjudicator/country ""}]
+              :competitor/activities []
+              :competition/classes []})))))
