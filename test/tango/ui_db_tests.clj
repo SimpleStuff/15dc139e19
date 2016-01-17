5aa42300185f35d8fd1237ddc6b03dda2d6db745
zclj <info-s@tryne.se>
2016-01-17 12:12:11 +0100

    wip on query

--- test/tango/ui_db_tests.clj
+++ test/tango/ui_db_tests.clj
@@ -7,11 +7,9 @@
  ;;                                                :db/valueType :db.type/ref}
                          
 
- ;;                          :competition/activities {:db/cardinality :db.cardinality/many
- ;;                                                   :db/valueType :db.type/ref}
 
- ;;                          :activity/source {:db/cardinality :db.cardinality/one
- ;;                                            :db/valueType :db.type/ref}
+
+
 
  ;;                          :round/dances  {:db/cardinality :db.cardinality/many
  ;;                                                   :db/valueType :db.type/ref}
@@ -19,7 +17,9 @@
  ;;                          ;:class/name {:db/unique :db.unique/identity}
  ;;                          }
 
-(def schema {:competition/name {:db/unique :db.unique/identity}
+(def schema {;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+             ;; Competition
+             :competition/name {:db/unique :db.unique/identity}
              
              :competition/adjudicators {:db/cardinality :db.cardinality/many
                                         :db/valueType :db.type/ref}
@@ -33,10 +33,70 @@
              :competition/activities {:db/cardinality :db.cardinality/many
                                       :db/valueType :db.type/ref}
 
+             :competition/classes {:db/cardinality :db.cardinality/many
+                                   :db/valueType :db.type/ref}
+
+             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+             ;; Adjudicator Panels
              :adjudicator-panel/adjudicators {:db/cardinality :db.cardinality/many
                                               :db/valueType :db.type/ref}
 
+             :adjudicator-panel/id {:db/unique :db.unique/identity}
+
+             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+             ;; Dance
+             :dance/name {:db/unique :db.unique/identity}
+
+             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+             ;; Adjudicator
              :adjudicator/id {:db/unique :db.unique/identity}
+
+             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+             ;; Participant
+             :participant/id {:db/unique :db.unique/identity}
+             
+             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+             ;; Activity
+
+             :activity/id {:db/unique :db.unique/identity}
+
+             :activity/source {:db/cardinality :db.cardinality/one
+                               :db/valueType :db.type/ref}
+
+             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+             ;; Rounds
+             :round/id {:db/unique :db.unique/identity}
+             
+             :round/panel {:db/cardinality :db.cardinality/one
+                           :db/valueType :db.type/ref}
+
+             :round/dances {:db/cardinality :db.cardinality/many
+                            :db/valueType :db.type/ref}
+
+             :round/results {:db/cardinality :db.cardinality/many
+                             :db/valueType :db.type/ref}
+
+             :round/starting {:db/cardinality :db.cardinality/many
+                              :db/valueType :db.type/ref}
+
+             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+             ;; Class
+             :class/id {:db/unique :db.unique/identity}
+
+             :class/dances {:db/cardinality :db.cardinality/many
+                            :db/valueType :db.type/ref}
+
+             :class/rounds {:db/cardinality :db.cardinality/many
+                            :db/valueType :db.type/ref}
+
+             :class/adjudicator-panel {:db/cardinality :db.cardinality/one
+                                       :db/valueType :db.type/ref}
+
+             :class/remaining {:db/cardinality :db.cardinality/many
+                               :db/valueType :db.type/ref}
+
+             :class/starting {:db/cardinality :db.cardinality/many
+                              :db/valueType :db.type/ref}
              })
 
 (defn- transact-small-example []
@@ -127,16 +187,102 @@ Checks that those adjudicators are the same as in the competition."
 (deftest query-for-competition-activities
   (testing "Query to get competition activities"
     (let [conn (transact-small-example)]
-      (is (= []
-             (db/query conn '[:find ?acts
+      ;; There should be two activites
+      (is (= 2
+             (count (db/query conn '[:find ?acts
+                                     :in $ ?competition-name
+                                     :where
+                                     [?e :competition/name ?competition-name]
+                                     [?e :competition/activities ?acts]]
+                              "TurboMegatävling"))))
+
+      ;; a rounds panel must be a competition panel and its adjudicators
+      ;;  must be a competition adjudicator
+      (is (= #{{:adjudicator-panel/name "2"}
+               {:adjudicator/name "Cesar"}
+               {:adjudicator/name "Bertil"}}
+             (into #{}
+                   (flatten
+                    (db/query conn '[:find
+                                     (pull ?p [:adjudicator-panel/name])
+                                     (pull ?adj [:adjudicator/name])
+                                     :in $ ?competition-name
+                                     :where
+                                     [?e :competition/name ?competition-name]
+                                     [?e :competition/activities ?act]
+                                     [?act :activity/source ?s]
+                                     [?s :round/panel ?p]
+                                     [?e :competition/panels ?p]
+                                     [?p :adjudicator-panel/adjudicators ?adj]
+                                     [?e :competition/adjudicators ?adj]]
+                              "TurboMegatävling")))))
+      
+      ;; round can contain dances from its class
+      (is (= 0
+             (db/query conn '[:find ?r
                               :in $ ?competition-name
                               :where
                               [?e :competition/name ?competition-name]
-                              [?e :competition/activities ?acts]]
-                       "TurboMegatävling"))))))
+                              [?e :competition/classes ?c]
+                              ;[?c :class/dances ?d]
+                              [?c :class/rounds ?r]
+                              ;[?r :round/dances ?d]
+                              ]
+                       "TurboMegatävling")))
+
+      ;; round participants must be in the rounds class
+      ;; and in the starting list
+      ;; and have a result
+
+      ;; judging adjudicators must be in round panel
+
+      
+      )))
 
 
 ;; :competition/classes
+(deftest query-for-competition-classes
+  (testing "Query to get classes"
+    (let [conn (transact-small-example)]
+      ;; A classes panels must be a competition panel and its adjudicators
+      ;; must be a competition adjudicator
+      (is (not (empty?
+                (db/query conn '[:find ?adj
+                                 :in $ ?competition-name
+                                 :where
+                                 [?e :competition/name ?competition-name]
+                                 [?e :competition/classes ?c]
+                                 [?e :competition/panels ?p]
+                                 [?c :class/adjudicator-panel ?p]
+                                 [?p :adjudicator-panel/adjudicators ?adj]
+                                 [?e :competition/adjudicators ?adj]]
+                          "TurboMegatävling"))))
+
+      ;; remaining participants must be in starting list
+      (is (not (empty?
+                (db/query conn '[:find [?p ...]
+                                 :in $ ?competition-name
+                                 :where
+                                 [?e :competition/name ?competition-name]
+                                 [?e :competition/classes ?c]
+                                 [?c :class/remaining ?p]
+                                 [?c :class/starting ?p]]
+                          "TurboMegatävling"))))
+
+      ;; remaining list can not be longer that starting
+      (is 
+       (let [[rem-count start-count]
+             (first (db/query conn '[:find (count ?r) (count ?s)
+                                     :in $ ?competition-name
+                                     :where
+                                     [?e :competition/name ?competition-name]
+                                     [?e :competition/classes ?c]
+                                     [?c :class/remaining ?r]
+                                     [?c :class/starting ?s]]
+                              "TurboMegatävling"))]
+         (>= start-count rem-count)))
+      )))
+
 
 ;; (deftest query-for-competition-panels
 ;;   (testing "Query to get adjudicators panels"
