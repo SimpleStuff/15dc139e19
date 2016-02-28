f687f44e21a9f120ff42fbbbe047771f2d696859
Marten Larsson <mala235813@gmail.com>
2016-02-28 05:14:19 -0800

    Refactor tango.export

--- src/tango/export.clj
+++ src/tango/export.clj
@@ -12,9 +12,82 @@
             ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; AdjPanelList/AdjList
+;; Utils
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn- old-make-adjudicator-node [adjudicator-info]
+(defmacro def- [item value]
+  `(def ^{:private true} ~item ~value))
+
+(defn- format-date [date]
+  (tf/unparse (tf/formatter "yyyy-MM-dd") (tf/parse date)) )
+
+(defn- b2i [b]
+  (if (true? b) 1 0))
+
+(defn- get-db [data]
+  (let [conn (d/create-conn uidb/schema)]
+    (d/transact! conn [(uidb/sanitize data)])
+    (d/db conn)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DancePerfect
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- make-dance-perfect-node [content]
+  (xml/element
+    :DancePerfect
+    {:Version "4.1"}
+    content))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DancePerfect/CompData
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- make-comp-data-node [query-result]
+  (let [result (first (first query-result))
+        options (:competition/options result)]
+    (xml/element
+      :CompData
+      {:Name               (:competition/name result)
+       :Date               (format-date (:competition/date result))
+       :Place              (:competition/location result)
+       :Org                ""
+       :AdjOrderFinal      (b2i (:dance-competition/adjudicator-order-final options))
+       :AdjOrderOther      (b2i (:dance-competition/adjudicator-order-other options))
+       :SameHeatAllDances  (b2i (:dance-competition/same-heat-all-dances options))
+       :RadomHeats         (b2i (:dance-competition/random-order-in-heats options))
+       :RandomHeats        (b2i (:dance-competition/random-order-in-heats options))
+       :PreView            (b2i (:printer/preview options))
+       :HeatText           (b2i (:dance-competition/heat-text-on-adjudicator-sheet options))
+       :NameOnNumberSign   (b2i (:dance-competition/name-on-number-sign options))
+       :ClubOnNumberSign   (b2i (:dance-competition/club-on-number-sign options))
+       :SkipAdjLetter      (b2i (:dance-competition/skip-adjudicator-letter options))
+       :PrinterSelectPaper (b2i (:printer/printer-select-paper options))
+       :ChineseFonts       (b2i (:presentation/chinese-fonts options))
+       :ArialFont          (:presentation/arial-font options)
+       :CourierFont        (:presentation/courier-font options)
+       }
+
+      )))
+
+(def- make-comp-data-node-query
+  '[:find (pull ?e [:competition/name
+                    :competition/date
+                    :competition/location
+                    :competition/options])
+    :where [?e :competition/name]]
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DancePerfect/AdjPanelList
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- make-adj-panel-list-node [content]
+  (xml/element
+    :AdjPanelList
+    {}
+    content))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DancePerfect/AdjPanelList/AdjList
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- make-adjudicator-node [adjudicator-info]
   (let [name (:adjudicator/name adjudicator-info)
         id (:adjudicator/id adjudicator-info)
         country (:adjudicator/country adjudicator-info)]
@@ -23,91 +96,89 @@
                                :Country country})
     ))
 
-(defn- old-make-adj-list-node [query-result]
+(defn- make-adj-list-node [query-result]
   (let [adjudicator-infos (map first query-result)]
     (xml/element
       :AdjList
       {:Qty (count adjudicator-infos)}
-      (map old-make-adjudicator-node (sort-by :adjudicator/id adjudicator-infos)))
+      (map make-adjudicator-node (sort-by :adjudicator/id adjudicator-infos)))
     ))
 
-(defn- old-export-adj-list [db]
-  (let [query-result (d/q '[:find (pull ?id [*])
-                            :where
-                            [?id :adjudicator/id]] db)]
-    (old-make-adj-list-node query-result)))
+(def- make-adj-list-node-query
+  '[:find (pull ?e [*])
+    :where [?e :adjudicator/id]])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; AdjPanelList/PanelList
+;; DancePerfect/AdjPanelList/PanelList
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn- old-make-panel-adj-node [query-result seq-number]
+(defn- make-panel-adj-node [query-result seq-number]
   (xml/element
     :PanelAdj
     {:Seq seq-number
      :AdjNumber (:adjudicator/id query-result)}))
 
-(defn- old-make-panel-node [panel seq-number]
+(defn- make-panel-node [panel seq-number]
   (let [adjudicator-infos (:adjudicator-panel/adjudicators panel)]
     (xml/element
       :Panel
       {:Seq seq-number
        :Qty (count adjudicator-infos)}
       (reduce
-        (fn [state adj-info] (conj state (old-make-panel-adj-node adj-info (count state))))
+        (fn [state adj-info] (conj state (make-panel-adj-node adj-info (count state))))
         []
         adjudicator-infos))))
 
-(defn- old-make-panel-list-node [adjudicator-panels]
-  (xml/element
-    :PanelList
-    {:Qty (count adjudicator-panels)}
-    (reduce (fn [elements panel]
-              (conj elements (old-make-panel-node panel (count elements))))
-            []
-            adjudicator-panels)
-  ))
-
-(defn- old-export-panel-list [db]
-  (let [qr (d/q '[:find (pull ?id [{:adjudicator-panel/adjudicators
-                                                            [:adjudicator/id]}])
-                                          :where
-                                          [?id :adjudicator-panel/id]] db)
-        adjudicator-panels (reduce conj [] (map first qr))
+(defn- make-panel-list-node [qr]
+  (let [adjudicator-panels (reduce conj [] (map first qr))
         n (- 30 (count adjudicator-panels))
         padded-panels (repeat n {:adjudicator-panel/adjudicators []})
         all-panels (into adjudicator-panels padded-panels)]
-    (old-make-panel-list-node all-panels)))
+    (xml/element :PanelList
+                 {:Qty (count all-panels)}
+                 (reduce
+                   (fn [elements panel]
+                     (conj elements (make-panel-node panel (count elements))))
+                   []
+                   all-panels))))
+
+(def- make-panel-list-node-query
+  '[:find (pull ?e [{:adjudicator-panel/adjudicators [:adjudicator/id]}])
+    :where [?e :adjudicator-panel/id]])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; AdjPanelList
+;; Export Definition
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn- old-export-adj-panel-list [db]
-  (xml/element
-    :AdjPanelList
-    {}
-    (old-export-adj-list db)))
+(def- export-definition
+  {
+   :xml-factory make-dance-perfect-node
+   :content [{:xml-factory make-comp-data-node
+              :query make-comp-data-node-query}
+             {:xml-factory make-adj-panel-list-node
+              :content [{:xml-factory make-adj-list-node
+                         :query       make-adj-list-node-query}
+                        {:xml-factory make-panel-list-node
+                         :query       make-panel-list-node-query}
+                        ]}
+             ]})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Utils
+;; Export
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn- get-db [data]
-  (let [conn (d/create-conn uidb/schema)]
-    (d/transact! conn [(uidb/sanitize data)])
-    (d/db conn)))
-
-(defn- old-export-dance-perfect [db]
-  (xml/element
-      :DancePerfect
-      {:Version "4.1"}
-      [(old-export-adj-panel-list db)
-       (old-export-panel-list db)]
-      ))
-
-(defn old-export [data]
-  (let [db (get-db data)]
-    (old-export-dance-perfect db) ;; TODO: def queries in file and pass in query result
+(defn- export-xml [db export-def]
+  (let [xml-factory (:xml-factory export-def)
+        query (:query export-def)
+        content (:content export-def)
+        export-xml-with-db (partial export-xml db)
+        ]
+    (if (nil? query)
+      (xml-factory (map export-xml-with-db content))
+      (xml-factory (d/q query db)))
     ))
 
-(xml/indent-str
-  (old-export u/expected-small-example))
+(defn export [data export-def file]
+  (let
+    [db (get-db data)]
+    (spit file (xml/emit-str (export-xml db export-def)))))
+
+(export u/expected-small-example export-definition "/tmp/baz.xml")
 
