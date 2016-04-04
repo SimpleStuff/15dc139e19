0e04a08d4e122e88e46cc7deda27153d213893cc
zclj <info-s@tryne.se>
2016-04-04 20:50:09 +0200

    Made judges selectable

--- src/tango/cljs/adjudicator/mutation.cljs
+++ src/tango/cljs/adjudicator/mutation.cljs
@@ -35,12 +35,6 @@
                ;(log online?)
                (d/transact! state [{:app/id 1 :app/online? online?}])))})
 
-;(defmethod mutate 'app/select-activity
-;  [{:keys [state]} _ {:keys [name]}]
-;  {:value  {:keys [:app/selected-activity]}
-;   :action (fn []
-;             (d/transact! state [{:app/id 1 :app/selected-activity name}]))})
-
 (defmethod mutate 'app/select-activity
   [{:keys [state]} _ {:keys [activity]}]
   {:value  {:keys [:app/selected-activity]}
@@ -49,11 +43,9 @@
                ;(log "Mutate")
                (d/transact! state [{:app/id 1 :app/selected-activity activity}])))})
 
-;(defmethod read :app/selected-activity
-;  [{:keys [state query]} _ _]
-;  {:value (do
-;            (log "Read selected-activity")
-;            (log query)
-;            {:name "Test"})
-;   :query true})
+(defmethod mutate 'app/select-adjudicator
+  [{:keys [state]} _ adjudicator]
+  {:value {:keys [:app/selected-adjudicator]}
+   :action (fn []
+             (d/transact! state [{:app/id 1 :app/selected-adjudicator adjudicator}]))})
 
