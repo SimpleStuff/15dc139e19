2514fda8f4ecca69466dc67c6364a8364c7356c8
zclj <info-s@tryne.se>
2016-04-10 19:58:32 +0200

    wip on setting marks on participants

--- src/tango/cljs/adjudicator/mutation.cljs
+++ src/tango/cljs/adjudicator/mutation.cljs
@@ -57,12 +57,13 @@
              (log result)
              (let [q
                    (d/transact! state [{:db/id              -1
-                                        :result/id (:result/id result)
+                                        ;:result/id (:result/id result)
                                         :result/mark-x      mark-x
                                         :result/participant [:participant/id (:result/participant result)]
                                         :result/activity [:activity/id (:result/activity result)]
                                         :result/adjudicator [:adjudicator/id (:result/adjudicator result)]}
                                        {:app/id 1 :app/results -1}
                                        ])]
-               (log q)))})
+               (log q)))
+   :remote true})
 
