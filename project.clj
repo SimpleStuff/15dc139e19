0385dbebae0be2dd8b665c43f63d901e4deea09b
zclj <info-s@tryne.se>
2016-05-02 17:40:36 +0200

    set all production builds

--- project.clj
+++ project.clj
@@ -81,9 +81,15 @@
                                :output-to     "resources/public/js/app.js"
                                :output-dir    "resources/public/js/out"
                                ;:source-map    "resources/public/js/out.js.map"
-                               :source-map    true
-                               :optimizations :none
-                               :pretty-print  true}}
+                               ;; PROD
+                               :optimizations :advanced
+                               :pretty-print  false
+
+                               ;; DEV
+                               ;:source-map true
+                               ;:pretty-print  true
+                               ;:optimizations :none
+                               }}
 
                {:id           "adj"
                 :source-paths ["src/tango/cljs/adjudicator" "src"]
@@ -112,13 +118,13 @@
                                :output-dir "resources/public/js/out/speaker"
 
                                ;; PROD
-                               ;:optimizations :advanced
-                               ;:pretty-print  false
+                               :optimizations :advanced
+                               :pretty-print  false
 
                                ;; DEV
-                               :source-map true
-                               :pretty-print  true
-                               :optimizations :none
+                               ;:source-map true
+                               ;:pretty-print  true
+                               ;:optimizations :none
                                }}
 
                ;; This next build is an compressed minified build for
