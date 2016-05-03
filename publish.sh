f1716652f674d4d7128ff42f4b1a579be542f310
mala <mala235813@gmail.com>
2016-05-03 21:59:20 +0200

    Version 0.1.1

--- publish.sh
+++ publish.sh
@@ -11,7 +11,7 @@ cd -
 echo "Copying uberjar"
 cp target/uberjar/tango-0.1.0-SNAPSHOT-standalone.jar ../tango-pub
 cd ../tango-pub
-git add tango-0.1.0-SNAPSHOT-standalone.jar
+git add tango-0.1.1-SNAPSHOT-standalone.jar
 
 echo "Commiting uberjar"
 git commit -m "latest build"
