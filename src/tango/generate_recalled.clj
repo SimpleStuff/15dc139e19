4c95b99ead8c6f2505aefb7f28f533240d2cbba7
zclj <info-s@tryne.se>
2016-05-05 21:20:56 +0200

    stubb on new html

--- src/tango/generate_recalled.clj
+++ src/tango/generate_recalled.clj
@@ -17,18 +17,17 @@
                                        classes))
         presentation (p/make-time-schedule-activity-presenter completed
                                                               class-for-round)
-        temp-result {:activity/name  (:activity/name completed)
+        temp-result {:activity/name   (:activity/name completed)
                      :activity/number (:activity/number completed)
-                     :round/recalled (map :result/participant-number
-                                          (filter #(not= (:result/recalled %) "")
-                                                  (:round/results round)))
+                     :round/recalled (filter #(not= (:result/recalled %) "")
+                                             (:round/results round))
                      :round/presentation presentation
                      :round/index (:round/index round)}]
     temp-result))
 
 (defn generate-html [recalled-info]
   (h/html [:head
-           [:title "Last Recalled"]
+           [:title " Last Recalled "]
            [:style {:type "text/css"} "#stor { Font-size: 54px; } #liten { Font-size: 36px; }"]]
 
           [:body
@@ -41,7 +40,84 @@
                         (if (= pres "Normal")
                           (str "Round " (inc (:round/index recalled-info)))
                           pres)))]]
-           [:h1 [:font {:id "stor"} (clojure.string/join ", " (:round/recalled recalled-info))]]
+           [:h1 [:font {:id "stor"} (clojure.string/join ", "
+                                                         (map :result/participant-number
+                                                              (:round/recalled recalled-info)))]]
+           [:br]
+           [:br]
+           [:br]
+           [:br]
+           [:br]]))
+
+;<html>
+;<head>
+;<title> Last Recalled </title>
+;</head>
+;<body>
+;<h1><font size=7>Event 4A</font></h1>
+;<h1><font size=7>Disco Par Guld B2</font></h1>
+;<h1><font size=7>Recalled from Semifinal</font></h1><br>
+;<table border=0 cellpadding=8 cellspacing=0>
+
+;<tr><td width=15%><h1><font size=7>2100</font></td><td width=65%><h1><font size=6>Clara Persson - Hanna Knutsson</font></h1></td><td width=20%><h1><font size=6>WAD/VDC</font></h1></td></tr>
+;</table>
+;<p><h1><font size=7>7 couples recalled</font></p>
+;<br><br><br><br>
+;<h1><font size=7>Event 4A</font></h1>
+;<h1><font size=7>Disco Par Guld B2</font></h1>
+;<h1><font size=6>Recalled from Semifinal</font></h1><br>
+;<h1><font size=7>
+;2090,  2092,  2094,  2095,  2097,  2098,  2100</font></h1><br><br><br><br><br><br>
+;
+;</body>
+;</html>
+
+(defn generate-re-html [recalled-info]
+  (h/html [:head
+           [:title " Last Recalled "]
+           ;[:style {:type "text/css"} "#stor { Font-size: 54px; } #liten { Font-size: 36px; }"]
+           ]
+
+          [:body
+           [:h1 [:font {:size 7} (str "Event " (:activity/number recalled-info))]]
+           [:h1 [:font {:size 7} (:activity/name recalled-info)]]
+           [:h1 [:font {:size 7}
+                 (str "Recalled from "
+                      (let [pres (:round
+                                   (:round/presentation recalled-info))]
+                        (if (= pres "Normal")
+                          (str "Round " (inc (:round/index recalled-info)))
+                          pres)))]]
+           [:table {:border 0 :cellpadding 8 :cellspacing 0}
+            (for [recalled (:round/recalled recalled-info)]
+              [:tr
+               [:td {:width "15%"} [:h1 [:font {:size 7} (:result/participant-number recalled)]]]
+               [:td {:width "65%"} [:h1 [:font {:size 6} "namn"                            ;(:participant/name (:result/participant recalled))
+                          ]]]
+               [:td {:width "20%"} [:h1 [:font {:size 6} "club"]]]] )]
+
+           [:p [:h1 [:font {:size 7} (str (count (:round/recalled recalled-info)) " couples recalled")]]]
+           [:br][:br][:br][:br]
+
+           [:h1 [:font {:size 7} (str "Event " (:activity/number recalled-info))]]
+           [:h1 [:font {:size 7} (:activity/name recalled-info)]]
+           [:h1 [:font {:size 6}
+                 (str "Recalled from "
+                      (let [pres (:round
+                                   (:round/presentation recalled-info))]
+                        (if (= pres "Normal")
+                          (str "Round " (inc (:round/index recalled-info)))
+                          pres)))]]
+           [:h1 [:font {:size 7} (clojure.string/join ", "
+                                                      (map :result/participant-number
+                                                           (:round/recalled recalled-info)))]]
+           ;<h1><font size=7>Event 4A</font></h1>
+           ;<h1><font size=7>Disco Par Guld B2</font></h1>
+           ;<h1><font size=6>Recalled from Semifinal</font></h1><br>
+           ;<h1><font size=7>
+           ;2090,  2092,  2094,  2095,  2097,  2098,  2100</font></h1><br><br><br><br><br><br>
+           ;
+           [:br]
            [:br]
            [:br]
            [:br]
@@ -53,8 +129,11 @@
   (let [completed (find-completed competition)
         recall-datas (mapv #(make-completed % competition) completed)]
     (mapv #(hash-map :activity/number (:activity/number %)
-                     :html (generate-html %))
-          recall-datas)))
+                     :html (generate-html %)
+                     :html-re (generate-re-html %))
+          recall-datas)
+    ;recall-datas
+    ))
 
 (defn write-recalled-html
   ;; Writes html files named 'ny_re_<round>.htm using data in the recalled-htmls and the write-fn
