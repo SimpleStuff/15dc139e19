ba04d5db651ce9627fd342ae60931aa1dc922411
zclj <info-s@tryne.se>
2016-05-10 20:14:18 +0200

    temp test cleaning

--- test/tango/export_tests.clj
+++ test/tango/export_tests.clj
@@ -1,9 +1,9 @@
-(ns tango.export-tests
-  (:require [clojure.test :refer :all]
-            [clj-time.core :as tc]
-            [clj-time.coerce :as tcr]
-            [tango.export :as e]
-            [tango.test-utils :as u]
-            [tango.expected.expected-small-result :as esr]
-            ))
+;(ns tango.export-tests
+;  (:require [clojure.test :refer :all]
+;            [clj-time.core :as tc]
+;            [clj-time.coerce :as tcr]
+;            [tango.export :as e]
+;            [tango.test-utils :as u]
+;            [tango.expected.expected-small-result :as esr]
+;            ))
 
